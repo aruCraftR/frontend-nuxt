23ce97455574898940e3ba7a15a92aba0903db5c
noeru-desu <3203911711@qq.com>
2026-01-08 22:39:52 +0800

    添加状态页跳转链接

--- app/layouts/empty.vue
+++ app/layouts/empty.vue
@@ -16,6 +16,8 @@ useHead({
             </template>
 
             <template #right>
+                <UButton icon="i-simple-icons-statuspage" color="neutral" variant="ghost"
+                    to="https://status.arucraftr.org" target="_blank" aria-label="Status Page" />
                 <UButton icon="i-simple-icons-qq" color="neutral" variant="ghost" to="https://re.arucraftr.org"
                     target="_blank" aria-label="QQ" />
             </template>
